b6a7d3dc333bb5e9d00f4d4f272e91d36269abd2
RAFAEL TRINDADE <rafaeltrindade649@gmail.com>
2017-04-21 13:41:08 -0300

    nova estrutura
    estrutura normalizada e com ligações

--- Reserva_de_vagas.sql
+++ Reserva_de_vagas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 14-Abr-2017 às 05:56
+-- Generation Time: 21-Abr-2017 às 18:55
 -- Versão do servidor: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -17,8 +17,19 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `reserva de vagas`
+-- Database: `aps-bd2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `acompanhante`
+--
+
+CREATE TABLE `acompanhante` (
+  `id_ac` int(3) NOT NULL,
+  `id_pessoa` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -27,37 +38,107 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
-  `Nome` varchar(20) COLLATE latin1_general_ci NOT NULL,
-  `Sobrenome` varchar(20) COLLATE latin1_general_ci NOT NULL,
-  `Nacionalidade` varchar(20) COLLATE latin1_general_ci NOT NULL DEFAULT 'Brasil',
-  `Nascimento` date NOT NULL,
-  `Sexo` enum('M','F') COLLATE latin1_general_ci DEFAULT NULL,
-  `Doc tipo` enum('RG','PASSPORT') COLLATE latin1_general_ci NOT NULL DEFAULT 'RG',
-  `Doc` varchar(10) COLLATE latin1_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
---
--- Extraindo dados da tabela `cliente`
---
-
-INSERT INTO `cliente` (`id`, `Nome`, `Sobrenome`, `Nacionalidade`, `Nascimento`, `Sexo`, `Doc tipo`, `Doc`) VALUES
-(1, 'Rafael', 'Trindade', 'Brasil', '1995-07-25', 'M', 'RG', '280178328');
+  `Nome` varchar(10) COLLATE latin1_general_ci DEFAULT NULL,
+  `id_cliente` int(3) NOT NULL,
+  `saida` date DEFAULT NULL,
+  `entrada` date DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `hoteis`
+-- Estrutura da tabela `contato`
 --
 
-CREATE TABLE `hoteis` (
-  `id` int(4) NOT NULL,
-  `Pais` varchar(20) COLLATE latin1_general_ci NOT NULL DEFAULT 'Brasil',
-  `Estado` varchar(2) COLLATE latin1_general_ci NOT NULL DEFAULT 'RJ',
-  `Cidade` varchar(50) COLLATE latin1_general_ci NOT NULL DEFAULT 'Rio de Janeiro',
-  `Classificação` enum('1','2','3','4','5') COLLATE latin1_general_ci NOT NULL COMMENT 'Estrelas',
-  `Local` point NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+CREATE TABLE `contato` (
+  `id_cont` int(3) NOT NULL,
+  `tel` varchar(13) COLLATE latin1_general_ci DEFAULT '(00)0000-0000',
+  `Email` varchar(25) COLLATE latin1_general_ci DEFAULT NULL,
+  `cel` varchar(14) COLLATE latin1_general_ci DEFAULT '(00)90000-0000',
+  `id_cliente` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `id_emp` int(3) NOT NULL,
+  `porte` enum('pequeno','médio','grande') COLLATE latin1_general_ci DEFAULT NULL,
+  `CNPJ` varchar(12) COLLATE latin1_general_ci DEFAULT NULL,
+  `id_cliente` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereço`
+--
+
+CREATE TABLE `endereço` (
+  `id-end` smallint(6) NOT NULL,
+  `Pais` varchar(10) COLLATE latin1_general_ci DEFAULT 'Brasil',
+  `Estado` varchar(3) COLLATE latin1_general_ci DEFAULT NULL,
+  `Cidade` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
+  `Cep` varchar(10) COLLATE latin1_general_ci NOT NULL DEFAULT '00.000-000',
+  `id_cliente` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id_fun` int(3) NOT NULL,
+  `id_pessoa` int(3) DEFAULT NULL,
+  `id_emp` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `hotel`
+--
+
+CREATE TABLE `hotel` (
+  `id_hotel` int(3) NOT NULL,
+  `Classificação` enum('1 estrela','2 estrelas','3 estrelas','4 estrelas','5 estrelas','6 estrelas') COLLATE latin1_general_ci DEFAULT NULL,
+  `estado` varchar(3) COLLATE latin1_general_ci DEFAULT NULL,
+  `cidade` varchar(15) COLLATE latin1_general_ci DEFAULT NULL,
+  `Email` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
+  `tel` varchar(13) COLLATE latin1_general_ci DEFAULT '(00)0000-0000',
+  `pais` varchar(10) COLLATE latin1_general_ci DEFAULT NULL,
+  `Qt_quartos` int(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `identificação`
+--
+
+CREATE TABLE `identificação` (
+  `id_id` int(3) NOT NULL,
+  `Sobrenome` varchar(15) COLLATE latin1_general_ci DEFAULT NULL,
+  `passaporte` varchar(12) COLLATE latin1_general_ci DEFAULT NULL,
+  `RG` varchar(12) COLLATE latin1_general_ci DEFAULT '00.000.000-0',
+  `id_pessoa` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa`
+--
+
+CREATE TABLE `pessoa` (
+  `id_pessoa` int(3) NOT NULL,
+  `id_cliente` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -66,50 +147,134 @@ CREATE TABLE `hoteis` (
 --
 
 CREATE TABLE `quarto` (
-  `Numero` int(4) NOT NULL,
-  `Tipo` enum('Casal','Grupo') COLLATE latin1_general_ci NOT NULL DEFAULT 'Casal',
-  `Lotação` int(2) NOT NULL,
-  `Diaria` decimal(6,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  `Diaria` decimal(5,2) DEFAULT NULL,
+  `num_quarto` smallint(4) NOT NULL,
+  `Andar` smallint(3) DEFAULT NULL,
+  `id_cliente` int(3) DEFAULT NULL,
+  `id_hotel` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `acompanhante`
+--
+ALTER TABLE `acompanhante`
+  ADD PRIMARY KEY (`id_ac`),
+  ADD KEY `id_pessoa` (`id_pessoa`);
+
+--
 -- Indexes for table `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Doc` (`Doc`);
+  ADD PRIMARY KEY (`id_cliente`);
 
 --
--- Indexes for table `hoteis`
+-- Indexes for table `contato`
 --
-ALTER TABLE `hoteis`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Local` (`Local`(25));
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id_cont`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id_emp`),
+  ADD UNIQUE KEY `CNPJ` (`CNPJ`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `endereço`
+--
+ALTER TABLE `endereço`
+  ADD PRIMARY KEY (`id-end`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id_fun`),
+  ADD KEY `id_emp` (`id_emp`),
+  ADD KEY `id_pessoa` (`id_pessoa`);
+
+--
+-- Indexes for table `hotel`
+--
+ALTER TABLE `hotel`
+  ADD PRIMARY KEY (`id_hotel`);
+
+--
+-- Indexes for table `identificação`
+--
+ALTER TABLE `identificação`
+  ADD PRIMARY KEY (`id_id`),
+  ADD UNIQUE KEY `RG` (`RG`),
+  ADD UNIQUE KEY `passaporte` (`passaporte`),
+  ADD KEY `id_pessoa` (`id_pessoa`);
+
+--
+-- Indexes for table `pessoa`
+--
+ALTER TABLE `pessoa`
+  ADD PRIMARY KEY (`id_pessoa`),
+  ADD KEY `id_cliente` (`id_cliente`);
 
 --
 -- Indexes for table `quarto`
 --
 ALTER TABLE `quarto`
-  ADD PRIMARY KEY (`Numero`);
+  ADD PRIMARY KEY (`num_quarto`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_hotel` (`id_hotel`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `acompanhante`
+--
+ALTER TABLE `acompanhante`
+  MODIFY `id_ac` int(3) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_cliente` int(3) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `hoteis`
+-- AUTO_INCREMENT for table `contato`
 --
-ALTER TABLE `hoteis`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `contato`
+  MODIFY `id_cont` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `endereço`
+--
+ALTER TABLE `endereço`
+  MODIFY `id-end` smallint(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id_fun` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `hotel`
+--
+ALTER TABLE `hotel`
+  MODIFY `id_hotel` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `identificação`
+--
+ALTER TABLE `identificação`
+  MODIFY `id_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pessoa`
+--
+ALTER TABLE `pessoa`
+  MODIFY `id_pessoa` int(3) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
